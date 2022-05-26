016f68fc08b421559cc090ebcb1034e0ef52fa4e
Phuc Tran <jamesptran96@gmail.com>
2022-05-25 21:14:35 -0700

    Add blueprint data for path

--- ml_toolbox/ml_toolbox.py
+++ ml_toolbox/ml_toolbox.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request, Response
 
+def create_app():
+    """an application factory reads config files and objects and create the
+    application
+    Parameters:
+        test_config (dict): The config object that overwrites other setting
+        for testing porposes
+    Returns:
+        Flask: a flask object application"""
 
-app = Flask(__name__)
-
-@app.route("/")
-def test():
-    return "<p>Hello, World!</p>"
+    app = Flask(__name__)
 
 
-@app.route("/home")
-def home():
-    return render_template('home.html')
+
+    @app.route("/home")
+    def home():
+        return render_template('home.html')
+
+
+    from .controllers import data_controller
+    app.register_blueprint(data_controller, url_prefix='/data')
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
