72e8e37f6cfbce7553ccb90e6ad07cf3af95a0e7
Ajan Subramanian <ajans1@uci.edu>
2022-05-25 22:09:33 -0700

    Updated data.py data folder

--- ml_toolbox/controllers/data.py
+++ ml_toolbox/controllers/data.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, g, jsonify, abort, current_app
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -9,20 +9,13 @@ data_controller = Blueprint('data', __name__)
 def download():
 
 	req_data = request.get_json()
-	train_tok = req_data['train_folder']
+	data_tok = req_data['data_folder']
 	gauth = GoogleAuth()
 	drive = GoogleDrive(gauth)
-	test_tok = req_data['test_folder']
-	train_list = drive.ListFile({'q': f"{train_tok} in parents and trashed=false"}).GetList()
-	test_list = drive.ListFile({'q': f"{test_tok} in parents and trashed=false"}).GetList()
-	for file in train_list:
+	data_list = drive.ListFile({'q': f"{data_tok} in parents and trashed=false"}).GetList()
+	for file in data_list:
 		print('title: %s, id: %s'% (file['title'],file['id']))
 		file.GetContentFile(file['title'])
-
-	for file in test_list:
-		print('title: %s, id: %s'% (file['title'],file['id']))
-		file.GetContentFile(file['title'])
-
 	resp = jsonify(success=True)
 	resp.status_code = 200
 	return resp
