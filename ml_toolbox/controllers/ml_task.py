4775ec56e01482c07f7f52cdad61e0c8d6aa3f6a
Ajan Subramanian <ajans1@uci.edu>
2022-05-27 21:05:03 -0700

    changed ml_task

--- ml_toolbox/controllers/ml_task.py
+++ ml_toolbox/controllers/ml_task.py
@@ -1,3 +1,13 @@
 from flask import Blueprint
+from flask import Blueprint, request, g, jsonify, abort, current_app
 
 mltask_controller = Blueprint('ml_task', __name__)
+
+@data_controller.route('/train', methods=['POST'])
+
+def train():
+	req_data = request.get_json()
+	print(req_data)
+	resp = jsonify(success=True)
+	resp.status_code = 200
+	return resp
