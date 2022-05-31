a722112034a4dcbb5337c4b4dbc81ad7da035fc6
Phuc Tran <jamesptran96@gmail.com>
2022-05-30 20:37:36 -0700

    Add visualization functionality

--- ml_toolbox/controllers/data.py
+++ ml_toolbox/controllers/data.py
@@ -1,15 +1,12 @@
-from flask import Blueprint, request, g, jsonify, abort, current_app
+from flask import Blueprint, request, g, jsonify, abort, current_app, send_file
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 data_controller = Blueprint('data', __name__)
 
 @data_controller.route('/download', methods=['POST'])
-
 def download():
-
 	req_data = request.get_json()
-	print(req_data)
 	full_path = req_data['data_folder']
 	data_tok = f"'{full_path.split('/')[-1]}'"
 	gauth = GoogleAuth()
@@ -25,7 +22,11 @@ def download():
 
 @data_controller.route('/upload', methods=['POST'])
 def upload():
-
 	resp = jsonify(success=True)
 	resp.status_code = 200
 	return resp
+
+
+@data_controller.route('/get_image/<name>')
+def get_image(name):
+	return send_file(name, mimetype='image/jpeg')
